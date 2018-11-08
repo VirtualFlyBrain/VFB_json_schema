4a27dc8ec5251ef7e1ed6850c9bd8705a86ff9b6
dosumis <dosumis@gmail.com>
2018-11-08 21:36:09 +0000

    Mostly tidying up and documenting.

--- src/query_roller.py
+++ src/query_roller.py
@@ -1,18 +1,26 @@
 from collections import namedtuple
 
-
 opm = namedtuple("opm", ["MATCH", "var", "RETURN", "LIMIT"])
-
-
-
-
 
 
 class QueryGenerator(object):
 
     def __init__(self):
 
-        ### Assumptions
+        ### Notes:
+        ###  queries have are all scoped to the object
+        ###  parts of queries are scoped to __init__
+        ###  QUeries are all opm objects
+        ### opm.MATCH = MATCH CLAUSE
+        ### opm.RETURN = *contents* of return/with CLAUSE
+        ### specific to this query
+        ### var = name of variable(s) returned by return clause.
+        ### If more than one separate by ','
+        ### the value of 'var' is used to specify an element of the WITH string between clauses.
+        ### LIMIT = False OR explicit additional WITH clause
+        ### specifying limit to number of results returned.
+        ### This WITH clause will be used as a filter before
+        ### the WITH/RETURN clause.
 
         pub = "{ pub_core: %s, microref: p.microref, PubMed: p.PMID, " \
               "FlyBase: p.FlyBase, DOI: p.DOI, ISBN: p.ISBN } " % self.roll_core("p")
@@ -20,7 +28,7 @@ class QueryGenerator(object):
         synonym = "{ label: rp.synonym, scope: rp.scope, type: rp.cat } "
 
         self.pub_syn = opm(MATCH="OPTIONAL MATCH (primary)-[rp:has_reference]->(p:pub)",
-                           RETURN= "COLLECT ( { pub: %s, synonym: %s } ) AS pub_syn" % (pub, synonym),
+                           RETURN="COLLECT ( { pub: %s, synonym: %s } ) AS pub_syn" % (pub, synonym),
                            var='pub_syn',
                            LIMIT=False)
 
@@ -29,19 +37,33 @@ class QueryGenerator(object):
             RETURN="COLLECT({ link: s.link_base + dbx.accession, link_text: s.label, " \
                    "site: %s, icon: coalesce(s.link_icon_url, '') }) AS xrefs" % self.roll_core("s"),
             var="xrefs",
-            LIMIT = False)
+            LIMIT=False)
 
         # Separate out parents from related?
 
-        self.rels = opm(
-                    var="rels",
-                    MATCH="OPTIONAL MATCH (o:Class)<-[r]-(primary) ",
-                    RETURN= "COLLECT ({ relation: r.label, object: %s } AS rels" % self.roll_core("o"),
-                    LIMIT = False)
+        self.Class_rels = opm(var="rels",
+                              MATCH="OPTIONAL MATCH (o:Class)<-[r { type: 'Related' }]-(primary) ",
+                              RETURN="COLLECT ({ relation: r.label, object: %s } AS rels" % self.roll_core("o"),
+                              LIMIT=False)
+
+        self.superClasses = opm(var="super",
+                                MATCH="OPTIONAL MATCH (o:Class)<-[r:SUBCLASSOF]-(primary) ",
+                                RETURN="COLLECT (%s) AS super" % self.roll_core("o"),
+                                LIMIT=False)
+
+        self.superTypes = opm(var="super",
+                              MATCH="OPTIONAL MATCH (o:Class)<-[r:INSTANCEOF]-(primary) ",
+                              RETURN="COLLECT (%s) AS super" % self.roll_core("o"),
+                              LIMIT=False)
+
+        self.ind_rels = opm(var="rels",
+                            MATCH="OPTIONAL MATCH (o:Individual { type: 'Related' })<-[r]-(primary) ",
+                            RETURN="COLLECT ({ relation: r.label, object: %s } AS rels" % self.roll_core("o"),
+                            LIMIT=False)
 
         image_match = "(i:Individual)<-[:depicts]-" \
-                 "(:Individual)-[irw:in_register_with]->(template:Individual)-[:depicts]->" \
-                 "(template_anat:Individual) "
+                      "(:Individual)-[irw:in_register_with]->(template:Individual)-[:depicts]->" \
+                      "(template_anat:Individual) "
 
         image_return = "{ template: template.label, folder: irw.folder, index: coalesce(irw.index, 0) }"
 
@@ -53,19 +75,21 @@ class QueryGenerator(object):
         )
 
         self.images = opm(
-            MATCH = "OPTIONAL MATCH (primary)<-[:SUBCLASSOF|INSTANCEOF*]-%s" % image_match,
-            RETURN = "COLLECT({ anatomy: %s, image: %s }) AS images " % (self.roll_core("i"), image_return),
-            var = "images",
-            LIMIT = "WITH template, irw, i, %s limit 5")  # Not keen on this hidden sub...
+            MATCH="OPTIONAL MATCH (primary)<-[:SUBCLASSOF|INSTANCEOF*]-%s" % image_match,
+            RETURN="COLLECT({ anatomy: %s, image: %s }) AS images " % (self.roll_core("i"), image_return),
+            var="images",
+            LIMIT="WITH template, irw, i, %s limit 5")  # Not keen on this hidden sub...
 
-        self.term = "{ core: %s, description: primary.description, comment: coalesce(primary.`annotation-comment`, [])} as term " % (
-        self.roll_core("primary"))
+        self.term = "{ core: %s, description: primary.description, " \
+                    "comment: coalesce(primary.`annotation-comment`, [])} " \
+                    "as term " % (self.roll_core("primary"))
         return
 
-
     def roll_core(self, var):
+        """Rolls core JSON (specifying minimal info about an entity.
+        var: the variable name for the entity within this cypher clause."""
         return "{ short_form: %s.short_form, label: %s.label, " \
-                "iri: %s.iri, types: labels(%s) } " % (var, var, var, var)
+               "iri: %s.iri, types: labels(%s) } " % (var, var, var, var)
 
     def roll_query(self, type, clauses, short_form, pretty_print=False):
 
@@ -74,22 +98,37 @@ class QueryGenerator(object):
         """
         var_stack = []
 
+        # May need to be more flexible here.  Allow primary query to be passed as arg?
         primary_query = "MATCH (primary:%s {short_form: '%s' }) " % (type, short_form)
 
         def roll_clause(q, primary="primary"):
+            """Rolls a single clause.
+            q = opm object specifying clause,
+            primary = var name for primary entity match default: 'primary'"""
+
+            # Make an internal copy of var_stack
+            # Add primary to this. (Primary should
+            # not be on var_stack as we need to keep
+            # it out of the final return clause.
             v = list(var_stack)
             v.append(primary)
             vars_string = ", ".join(v)
             delim = " "
+            # Output items added to stack in order
+            # Will be strung toghether later with spec' d delim
             out_list = []
             if pretty_print:
                 delim = " \n"
-                out_list.append("")
+                out_list.append("")  # Adds an extra return
+            # MATCH goes first
             out_list.append(q.MATCH)
+            # Then LIMIT if there is one
             if q.LIMIT:
                 out_list.append(q.LIMIT % vars_string)
+            # then WITH (RETURN clause)
             out_list.append("WITH " + q.RETURN + ", " + vars_string)
             out = delim.join(out_list) + delim
+            # Update var stack for next round
             if q.var:
                 var_stack.append(q.var)
             return out
@@ -107,12 +146,13 @@ class QueryGenerator(object):
     def anatomical_ind_query(self, short_form, pretty_print=False):
         return self.roll_query(type='Individual:Anatomy',
                                short_form=short_form,
-                               clauses=[self.xrefs],
-                               pretty_print=pretty_print) # Is Anatomy label sufficient here
+                               clauses=[self.xrefs,
+                                        self.image],
+                               pretty_print=pretty_print)  # Is Anatomy label sufficient here
 
     def class_query(self, short_form, pretty_print=False):
         return self.roll_query(type='Class:Anatomy',
-                               short_form = short_form,
+                               short_form=short_form,
                                clauses=[self.xrefs,
                                         self.images],
                                pretty_print=pretty_print)
@@ -123,15 +163,5 @@ class QueryGenerator(object):
                                clauses=[self.xrefs],
                                pretty_print=pretty_print)
 
-
-
-
-
-
-
 # Really need an edge property that distinguishes logical from annotation properties!
 # TODO - Add retain related property to edge flipping code.Nico to add to V2.
-
-
-
-
