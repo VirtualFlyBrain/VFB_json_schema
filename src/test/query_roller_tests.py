4d32347e70ab408a0166b481e0d03bf7b9010eb0
dosumis <dosumis@gmail.com>
2018-11-09 17:42:35 +0000

    Refactored & fixed general testing framework.

--- src/test/query_roller_tests.py
+++ src/test/query_roller_tests.py
@@ -9,14 +9,27 @@ class test_wrapper():
         self.V = get_validator("../../json_schema/vfb_termInfo.json")
         self.nc = neo4j_connect('http://pdb.virtualflybrain.org', 'neo4j', 'neo4j')
 
-    def test(self, query):
-        results = False
+    def test(self, t, query, single=True, print_result=True):
+
+        """Runs Cypher query.
+            Tests if query failed else
+            Tests length of results,
+            Tests results validity against schema.
+            query = cypher query,
+            t = QueryRollerTest object,
+            single = True if results list length should be one
+            print results: wot it sez on the tin"""
         s = self.nc.commit_list([query])
+        t.assertTrue(s, "Cypher query failed.")
         if s:
             results = results_2_dict_list(s)
-            validate(self.V, results[0])
-        return results
-
+            if print_result: print(results)
+            if single: t.assertEqual(len(results), 1)
+            validation_status = validate(self.V, results[0])
+            t.assertTrue(validation_status)
+            return results
+        else:
+            return False
 
 
 class QueryRollerTest(unittest.TestCase):
@@ -30,79 +43,80 @@ class QueryRollerTest(unittest.TestCase):
         query = self.qg.roll_query(types=["Class"],
                                    clauses=[],
                                    short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r, "Simple class query returns no results.")
+        r = self.qw.test(t=self,
+                         query=query)
+
 
     def test_individual_term(self):
         query = self.qg.roll_query(types=["Individual"],
                                    clauses=[],
                                    short_form='VFB_00011179')
-        r = self.qw.test(query)
-        self.assertTrue(r, "Simple class query returns no results.")
+        r = self.qw.test(t=self,
+                         query=query)
 
 
     def test_class_images(self):
         query = self.qg.roll_query(types=["Class"],
                                    clauses=[self.qg.images],
                                    short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
     def test_class_xrefs(self):
         query = self.qg.roll_query(types=["Class"],
                                    clauses=[self.qg.xrefs],
                                    short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
 
     def test_class_parents(self):
         query = self.qg.roll_query(types=["Class"],
                                    clauses=[self.qg.parents],
                                    short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
     def test_class_relationships(self):
         query = self.qg.roll_query(types=["Class"],
                                    clauses=[self.qg.relationships],
                                    short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
 
     def test_individual_relationships(self):
         query = self.qg.roll_query(types=["Individual"],
                                    clauses=[self.qg.relationships],
                                    short_form='VFB_00011179')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
 
     def test_individual_parents(self):
         query = self.qg.roll_query(types=["Individual"],
                                    clauses=[self.qg.relationships],
                                    short_form='VFB_00011179')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
     def test_individual_image(self):
         query = self.qg.roll_query(types=["Individual"],
                                    clauses=[self.qg.image],
                                    short_form='VFB_00011179')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
     def test_class(self):
         query = self.qg.class_query(short_form='FBbt_00007422')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
 
     def test_individual(self):
         query = self.qg.anatomical_ind_query(short_form='VFB_00011179')
-        r = self.qw.test(query)
-        self.assertTrue(r)
+        r = self.qw.test(t=self,
+                         query=query)
 
 
 
