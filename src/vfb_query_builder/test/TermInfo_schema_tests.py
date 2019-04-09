c4d41cd993280a11083b7a2d19fad98c01045a9a
Rob Court <rcourt@ed.ac.uk>
2019-04-09 18:44:48 +0100

    case fix

--- src/vfb_query_builder/test/TermInfo_schema_tests.py
+++ src/vfb_query_builder/test/TermInfo_schema_tests.py
@@ -7,7 +7,7 @@ class TermInfoRollerTest(unittest.TestCase):
 
     def setUp(self):
         self.ql = QueryLibrary()
-        self.qw = TestWrapper('vfb_terminfo.json')
+        self.qw = TestWrapper('vfb_termInfo.json')
         print("Running", self.id().split('.')[1:])
 
     def test_class_term(self):
