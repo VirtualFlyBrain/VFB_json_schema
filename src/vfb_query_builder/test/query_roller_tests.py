9e272cf34cea3c089a74c21e60a5b81c47ebbb26
Rob Court <rcourt@ed.ac.uk>
2022-03-31 17:13:50 +0100

    returning complete xmi for copy and paste

--- src/vfb_query_builder/test/query_roller_tests.py
+++ src/vfb_query_builder/test/query_roller_tests.py
@@ -34,7 +34,9 @@ class QueryRollerTest(unittest.TestCase):
         # extract out the cypher queries from the ecore vfb.xmi file into model list
         ecore=urllib.request.urlopen("https://github.com/VirtualFlyBrain/geppetto-vfb/raw/master/model/vfb.xmi")
         model = []
+        xmi = []
         for line in ecore:
+            xmi.append(line.decode('utf-8'))
             if "query=" in line.decode('utf-8') and "&quot;statement&quot;" in line.decode('utf-8'):
                 model.append(line.decode('utf-8').split('"')[1])
         for query in queries:
@@ -44,6 +46,12 @@ class QueryRollerTest(unittest.TestCase):
                 if query in cypher:
                     print(oldquery(cypher))
                     print(get_edits_string(cypher, queries[query]))
-    
+                    for line in xmi:
+                        if cypher in line:
+                            line = line.replace(cypher, queries[query])
+                            break
+        print('New vfb.xmi')
+        print(xmi.join('\n'))
+                            
 if __name__ == '__main__':
     unittest.main()
