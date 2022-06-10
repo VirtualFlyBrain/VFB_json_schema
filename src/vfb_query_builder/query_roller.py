95c2a3c494670af6725a78d54c5cb043dfc0ee76
Rob Court <rcourt@ed.ac.uk>
2022-06-10 16:09:01 +0100

    fix indent

--- src/vfb_query_builder/query_roller.py
+++ src/vfb_query_builder/query_roller.py
@@ -700,8 +700,8 @@ class QueryLibrary(QueryLibraryCore):
                              clauses=[self.term(),
                                       self.anatomy_channel_image()],
                              pretty_print=True)
-    
-   def dataset_image_query(self, short_forms: List):
+
+    def dataset_image_query(self, short_forms: List):
         return query_builder(query_short_forms=short_forms,
                              query_labels=['Individual'],
                              clauses=[self.term(),
