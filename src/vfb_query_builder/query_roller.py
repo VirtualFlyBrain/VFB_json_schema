23b5d8dc2b5adfefb563c03942810b41dd9d9e82
hkir-dev <huseyin_kir@yahoo.com>
2022-12-13 14:33:18 +0000

    update relation query to handle hidden relations

--- src/vfb_query_builder/query_roller.py
+++ src/vfb_query_builder/query_roller.py
@@ -230,9 +230,9 @@ class QueryLibraryCore:
 
     def relationships(self): return (Clause(vars=["relationships"],
                                             MATCH=Template("OPTIONAL MATCH "
-                                                           "(o:Class)<-[r {type:'Related'}]-($pvar$labels)" 
-                                                           "where (not (r.hide_in_terminfo[0] = true) OR "
-                                                           "(not exists(r.hide_in_terminfo))) "),
+                                                           "(o:Class)<-[r {type:'Related'}]-($pvar$labels) " 
+                                                           "where (not exists(r.hide_in_terminfo)) OR "
+                                                           "(not (r.hide_in_terminfo[0] = true)) "),
                                             WITH="CASE WHEN o IS NULL THEN [] "
                                                  "ELSE COLLECT ({ relation: %s, object: %s }) "
                                                  "END AS relationships " % (roll_min_edge_info("r"),
