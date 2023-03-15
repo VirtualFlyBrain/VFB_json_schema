88537b47d2de5b40d841e3fdfbe0880efa8736eb
Rob Court <rcourt@ed.ac.uk>
2023-03-15 13:20:06 +0000

    typo fix

--- src/vfb_query_builder/query_roller.py
+++ src/vfb_query_builder/query_roller.py
@@ -412,7 +412,7 @@ class QueryLibraryCore:
         MATCH=Template(
             "MATCH (channel:Individual)<-[irw:in_register_with]-"
             "(channel:Individual)-[:depicts]->($pvar$labels)"),
-        WITH="{index: [] + coalesce(pd.irw.index, []), "
+        WITH="{index: [] + coalesce(irw.index, []), "
              "extent: ([]+irw.extent)[0], center: ([]+irw.center)[0], voxel: ([]+irw.voxel)[0], "
              "orientation: coalesce(([]+irw.orientation)[0], ''), "
              "image_folder: coalesce(([]+irw.folder)[0],''), "
